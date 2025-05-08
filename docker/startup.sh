cfac4dad981beb46a13ce886c5ef0529d5e30932
Shaon Majumder <smazoomder@gmail.com>
2025-05-08 15:14:15 +0600

    laravel installation dynamic

---
+++ docker/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /var/www/html
+
+# If artisan not found, assume Laravel not present
+if [ ! -f artisan ]; then
+    echo "Laravel not found. Creating Laravel in /tmp/laravel..."
+    composer create-project --prefer-dist laravel/laravel /tmp/laravel
+
+    echo "Copying Laravel to current directory..."
+    cp -R /tmp/laravel/. .
+    rm -rf /tmp/laravel
+else
+    echo "Laravel already exists. Skipping creation."
+fi
+
+# Continue standard setup
+# composer install --no-interaction --prefer-dist --optimize-autoloader
+# php artisan package:discover
+
+exec "$@"
