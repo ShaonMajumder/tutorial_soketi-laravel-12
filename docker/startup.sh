fa099ba80649c898690c0884958e9abf3f6c408b
Shaon Majumder <smazoomder@gmail.com>
2025-05-24 11:58:04 +0600

    .env issue solve

--- docker/startup.sh
+++ docker/startup.sh
@@ -7,6 +7,10 @@ if [ ! -f artisan ]; then
     echo "Laravel not found. Creating Laravel in /tmp/laravel..."
     composer create-project --prefer-dist laravel/laravel /tmp/laravel
 
+    echo "Copying .env from environment directory..."
+    cp environment/.env /tmp/laravel/.env
+    php artisan config:clear
+
     echo "Copying Laravel to current directory..."
     cp -R /tmp/laravel/. .
     rm -rf /tmp/laravel
@@ -18,12 +22,12 @@ echo "Setting correct permissions for the storage directory..."
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 775 /var/www/html/storage
 
-if [ -f environment/.env ]; then
-    echo "Copying .env from environment directory..."
-    cp environment/.env .env
-    php artisan config:clear
-else
-    echo "Warning: environment/.env not found, skipping .env copy."
-fi
+# if [ -f environment/.env ]; then
+#     echo "Copying .env from environment directory..."
+#     cp environment/.env .env
+#     php artisan config:clear
+# else
+#     echo "Warning: environment/.env not found, skipping .env copy."
+# fi
 
 exec "$@"
