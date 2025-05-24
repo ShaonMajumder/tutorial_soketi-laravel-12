942c7c0676d13a85a0463f1dc87e4b38882676f1
Shaon Majumder <smazoomder@gmail.com>
2025-05-24 11:59:06 +0600

    .env issue solve

--- docker/startup.sh
+++ docker/startup.sh
@@ -22,12 +22,4 @@ echo "Setting correct permissions for the storage directory..."
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 775 /var/www/html/storage
 
-# if [ -f environment/.env ]; then
-#     echo "Copying .env from environment directory..."
-#     cp environment/.env .env
-#     php artisan config:clear
-# else
-#     echo "Warning: environment/.env not found, skipping .env copy."
-# fi
-
 exec "$@"
